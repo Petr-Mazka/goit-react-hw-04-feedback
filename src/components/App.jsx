cf39112a0af9d0a50bcfaf3e9065ada423143224
Petr Mazka <petr.mazka@gmail.com>
2022-07-26 18:50:37 +0300

    1
    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,5 @@
+import Counter from "./Saa";
+
 export const App = () => {
   return (
     <div
@@ -10,7 +12,7 @@ export const App = () => {
         color: '#010101'
       }}
     >
-      React homework template
+      <Counter step={5} />
     </div>
   );
 };
